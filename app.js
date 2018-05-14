1a2a5c5b81879daede84fd7085972175e722c387
wgbusch <w.g.busch2@gmail.com>
2018-05-14 04:23:00 -0300

    ok

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('../routes/index');
-var usersRouter = require('../routes/users');
-var addNewComboRouter = require('../routes/addnewcombo');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var addNewComboRouter = require('./routes/addnewcombo');
 
 var app = express();
 
