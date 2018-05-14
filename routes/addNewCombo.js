d3234d4028a272147481fa065b6247431469a227
wgbusch <w.g.busch2@gmail.com>
2018-05-14 12:13:15 -0300

    ok

--- routes/addNewCombo.js
+++ routes/addNewCombo.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var addNewComboControllers = require("../controllers/addNewComboControllers");
 
 router.post('/', addNewComboControllers.handlePostRequest);
-
 router.get('/', addNewComboControllers.handleGetRequest);
 
 module.exports = router;
